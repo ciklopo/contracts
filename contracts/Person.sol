87e1c678e81aaf8f58ba360e2ea68d65450c57ff
Merlin <merlinpro@MacBook-Pro-de-Merlin.local>
2017-10-24 20:50:54 -0500

    Changing the test and Person Contract
    	renamed:    .gitignore.txt -> .gitignore
    	new file:   contracts/Person.sol
    	new file:   migrations/2_deploy_contracts.js
    	new file:   test/01_person.js

---
+++ contracts/Person.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.2;
+
+contract Person {
+	string public name;
+
+	function setName(string _name){
+		name = _name;
+	}
+}
---
+++ test/01_person.js
@@ -0,0 +1,53 @@
+var Person = artifacts.require("Person");
+expect = require ("chai").expect;
+
+contract("Testing the Person contract", function (accounts) {
+	describe("Deploy the Person contract" , function () {
+		it ("Get an instance of the Person contract", function(){
+			return Person.new().then( function (instance){
+				personContract = instance;
+			})
+		});
+	});
+
+	describe("Test the contract variables", function() {
+		describe("Variable: name", function() {
+		    it("Use setName to set a first name" , function(){
+		        return personContract.setName("María").then ( function(response){
+		            expect(response).to.not.be.an("error");
+		        });
+		    });
+
+            it("Check the first name was set properly" , function(){
+                return personContract.name().then ( function(response){
+                    expect(response.toString()).to.be.equal("María");
+                });
+            });
+
+            it("Use setName to set a second name" , function(){
+                return personContract.setName("Juan").then ( function(response){
+                    expect(response.toString()).to.not.be.an("error");
+                });
+            });
+
+            it("Check the second name was set properly" , function(){
+                return personContract.name().then ( function(response){
+                    expect(response.toString()).to.be.equal("Juan");
+                });
+            });
+		});
+	});
+});
+
+/*
+
+	describe("Test the contract variables", function() {
+
+		it("The name variable is Ciklopo" , function(){
+			return personContract.name().then ( function(response) {
+				expect(response.toString()).to.be.equal("Ciklopo");
+			});
+		});
+	});
+
+*/
